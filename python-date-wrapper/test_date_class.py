d531c075ee44a7fe76a0bb613a1f2babb2a6091e
Emily <Emily.Olsen.Colonthree@outlook.com>
2026-09-17 19:35:08 -0400

    Tested that ValueError is thrown as needed.

--- python-date-wrapper/test_date_class.py
+++ python-date-wrapper/test_date_class.py
@@ -1,8 +1,9 @@
 import unittest
 from date_class import Date
 
+# Tests if default Date constructor functions properly
 class DateConstructorTest(unittest.TestCase):
-    def setUp(self):
+    def setUp(self): # Creates the Default Date to be tested
         self.date = Date()
 
     def test_day(self):
@@ -13,6 +14,35 @@ class DateConstructorTest(unittest.TestCase):
 
     def test_year(self):
         self.assertEqual(self.date.year, 1900)
+
+# Creates a valid Date object, then tests that the right values have been used
+class ValidDateConstructorTest(unittest.TestCase):
+    def setUp(self):
+        self.date = Date(9, 15, 2026) # Date is 9/15/2026
+
+    def test_day(self):
+        self.assertEqual(self.date.day, 15)
+
+    def test_month(self):
+        self.assertEqual(self.date.month, 9)
+
+    def test_year(self):
+        self.assertEqual(self.date.year, 2026)
+
+class InvalidMonth(unittest.TestCase):
+    def test_ValueError(self):
+        with self.assertRaises(ValueError):
+            self.date = Date(0, 2, 2009)
+
+class InvalidDay(unittest.TestCase):
+    def test_ValueError(self):
+        with self.assertRaises(ValueError):
+            self.date = Date(3, 32, 2000)
+
+class InvalidLeapDay(unittest.TestCase):
+    def test_ValueError(self):
+        with self.assertRaises(ValueError):
+            self.date = Date(2, 29, 2100)
 
 if __name__ == '__main__':
     unittest.main()
